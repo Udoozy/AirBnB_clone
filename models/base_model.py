6b9d8e2dd7b3a1274512afc78b51f5a2e6449c79
Udoozy <agbo4samson@gmail.com>
2025-10-23 23:21:02 +0100

    models/base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,8 +2,6 @@
 """
 Module for AirBnB clone
 """
-
-import models
 from uuid import uuid4
 from datetime import datetime
 
@@ -12,50 +10,24 @@ class BaseModel:
     """
     Basemodel for all the classes
     """
-
-    def __init__(self, *args, **kwargs):
-        """
-        Initializing the AirBnb class
-        """
-
-        date_format = '%Y-%m-%dT%H:%M:%S.%f'
-        if not kwargs:
-            self.id = str(uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-            models.storage.new(self)
-        else:
-            for i, j in kwargs.items():
-                if i in ("updated_at", "created_at"):
-                    self.__dict__[i] = datetime.strptime(j, date_format)
-                elif i[0] == "id":
-                    self.__dict__[i] = str(j)
-                else:
-                    self.__dict__[i] = j
+    def __init__(self):
+        """Innitializing the atrributes"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        """
-        Returns it string repr
-        """
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
-
+        """String formated"""
+        return f"[BaseModel] ({self.id}) {self.__dict__}"
+  
     def save(self):
-        """
-        Updaate the  updated_at with the current datetime
-        """
-        self.updated_at = datetime.utcnow()
-        models.storage.save()
+        """save mothod"""
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        """
-        Returns a dictionary containing all keys and value
-        """
-        my_dict = {}
-        for i, j in self.__dict__.items():
-            if i == "created_at" or i == "updated_at":
-                my_dict[i] = j.isoformat()
-            else:
-                my_dict[i] = j
-        my_dict["__class__"] = self.__class__.__name__
-        return my_dict
+        """Method that converts to dictionary"""
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
