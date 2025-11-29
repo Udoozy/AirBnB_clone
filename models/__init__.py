86555e94d28a51284911570d3b4e068f99f5268c
Udoozy <agbo4samson@gmail.com>
2025-11-29 16:25:50 +0100

    Update to models

--- models/__init__.py
+++ models/__init__.py
@@ -3,11 +3,21 @@
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 classes = {
     "BaseModel": BaseModel,
-    "User": User
+    "User": User,
+    "City": City,
+    "Place": Place,
+    "State": State,
+    "Amenity": Amenity,
+    "Review": Review
 }
 
 storage = FileStorage()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,10 +2,20 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 classes = {
     "BaseModel": BaseModel,
-    "User": User
+    "User": User,
+    "City": City,
+    "Place": Place,
+    "State": State,
+    "Amenity": Amenity,
+    "Review": Review
 }
 
 
